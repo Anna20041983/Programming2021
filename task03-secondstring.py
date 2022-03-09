0a92182a198db56dc1b887c687b9415bfb429fb5
Anna20041983 <98349435+Anna20041983@users.noreply.github.com>
2022-03-09 06:25:23 +0000

    Add files via upload

---
+++ task03-secondstring.py
@@ -0,0 +1,6 @@
+# Program creating a string and outputting every second letter in reverse order
+# Author: Anna Kozakiewicz
+
+inputString = input('Please enter a sentence:')
+reversed = inputString[::-2]
+print(reversed)
